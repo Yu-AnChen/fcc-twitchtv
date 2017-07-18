3e4e83f4324c43cf1117adc3cfe5474cbc20cf57
Yu-An Chen <atwood12@gmail.com>
2017-07-18 18:30:28 -0500

    add http and service module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
 	BrowserModule,
 	BrowserAnimationsModule,
+	HttpModule,
 	AngularFontAwesomeModule
   ],
   providers: [],
